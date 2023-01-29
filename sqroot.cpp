928fe610f669c1b80a4344bf6e8acf183fca7f3f
MANSI SWARAJ <mansiswaraj26@gmail.com>
2023-01-29 15:48:35 +0530

    sqrt

---
+++ sqroot.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"ENTER A NUMBER :"<<endl;
+    cin>>a;
+    int sqroot=sqrt(a);
+    cout<<sqroot<<endl;
+    return 0;
+}
